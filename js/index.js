654cf202c3cf485e53f6bf49235b7ef8723049b6
Murilo <murilo.mello27@gmail.com>
2021-09-22 21:46:09 -0300

    bikecraft

---
+++ js/index.js
@@ -0,0 +1,16 @@
+if(window.SimpleSlide){
+    
+    new SimpleSlide({
+        slide: 'porti', // nome do atributo data-slide="principal"
+        auto: true, // se o slide deve passar automaticamente
+        time: 4000, // tempo de transição dos slides
+      });
+    
+      new SimpleSlide({
+        slide: 'portii', // nome do atributo data-slide="principal"
+        auto: true, // se o slide deve passar automaticamente
+        time: 4000, // tempo de transição dos slides
+        nav: true
+      });
+}
+
